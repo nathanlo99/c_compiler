3b172c264edd07e3a8723b30e58d2e1f967426c0
Nathan Lo <nathanlo99@hotmail.ca>
2023-03-31 03:06:42 -0400

    Move files into folders

--- tests/constant_folding.c
+++ tests/constant_folding.c
@@ -1,10 +1,2 @@
 
-int wain(int a, int b) {
-  int c = 0;
-  int *d = NULL;
-  while (c < 3 * 4 + 2 * 12) {
-    d = d + (2 - 3);
-    c = c + (1 + 1 + 1);
-  }
-  return 20 * 3 + c;
-}
+int wain(int a, int b) { return a + 1 + 1 + 1; }
