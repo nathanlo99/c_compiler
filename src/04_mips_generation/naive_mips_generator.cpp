23e0d93b8059fbf7bfbf0a3040181bfb6c876f34
Nathan Lo <nathanlo99@hotmail.ca>
2023-04-07 01:09:53 -0400

    Untested optimization to MIPS generation

--- src/04_mips_generation/naive_mips_generator.cpp
+++ src/04_mips_generation/naive_mips_generator.cpp
@@ -100,18 +100,17 @@ void NaiveMIPSGenerator::visit(Procedure &procedure) {
   table.leave_procedure();
 }
 
-void NaiveMIPSGenerator::visit(VariableLValueExpr &expr) {
-  const int offset = table.get_offset(expr.variable);
-  load_const(3, offset);
-  add(3, 29, 3);
-  annotate("Grabbing address of lvalue " + expr.variable.name + ": offset " +
-           std::to_string(offset));
+void NaiveMIPSGenerator::visit(VariableLValueExpr &) {
+  unreachable("Variable lvalue code is handled in addressof and assignment");
+  // const int offset = table.get_offset(expr.variable);
+  // load_const(3, offset);
+  // add(3, 29, 3);
+  // annotate("Grabbing address of lvalue " + expr.variable.name + ": offset " +
+  //          std::to_string(offset));
 }
 
 void NaiveMIPSGenerator::visit(DereferenceLValueExpr &expr) {
-  // Since an l-value puts its address into $3, dereferencing it as an lvalue
-  // is a no-op
-  expr.argument->accept_simple(*this);
+  unreachable("Dereference lvalue code is handled in addressof and assignment");
 }
 
 void NaiveMIPSGenerator::visit(TestExpr &expr) {
@@ -221,7 +220,7 @@ void NaiveMIPSGenerator::visit(AddressOfExpr &expr) {
     add(3, 3, 29);
   } else if (const auto lhs = std::dynamic_pointer_cast<DereferenceLValueExpr>(
                  expr.argument)) {
-    expr.argument->accept_simple(*this);
+    lhs->argument->accept_simple(*this);
   } else {
     unreachable("Unknown lvalue type");
   }
@@ -281,7 +280,7 @@ void NaiveMIPSGenerator::visit(AssignmentStatement &statement) {
   } else if (const auto lvalue =
                  std::dynamic_pointer_cast<DereferenceLValueExpr>(
                      statement.lhs)) {
-    statement.lhs->accept_simple(*this);
+    lvalue->argument->accept_simple(*this);
     push(3);
     statement.rhs->accept_simple(*this);
     pop(5);
